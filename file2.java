47c82aef27ac809b0d3ec842baf654aa4e258d92
srisai <srisa@gmail.com>
2025-04-11 01:36:27 -0500

    new

--- file2.java
+++
@@ -1,9 +0,0 @@
-public class MyClass{
-   public static void main(String[] args)
-   {
-     System.out.println("Hello World!);
-   }
-}
-}
-i love my india 
-   i hope you understand
--- file5.java
+++
@@ -1,9 +0,0 @@
-public class MyClass{
-   public static void main(String[] args)
-   {
-     System.out.println("Hello World!);
-     System.out.println("1st statement !");
-      System.out.println("2st statement !");
-   }
-}
-
